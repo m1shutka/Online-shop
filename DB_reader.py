a8de2c36b6fe9a0af9e88987cdfa348f21997318
Michael Mihailovsky <113742674+m1shutka@users.noreply.github.com>
2023-11-29 00:08:57 +0700

    Add files via upload

---
+++ DB_reader.py
@@ -0,0 +1,173 @@
+﻿import sqlite3
+
+class DbReader():
+    def __init__(self):
+        self.__db = sqlite3.connect('shop.db', check_same_thread=False)
+
+
+
+    #Методы для товаров
+    def get_all_items(self):
+        """Получение списка всех товаров"""
+        cursor = self.__db.cursor()
+        return cursor.execute("SELECT * FROM items").fetchall()
+    
+    def add_item(self, name, price, photo):
+        """Добавление новго товара"""
+        cursor = self.__db.cursor()
+        cursor.execute("INSERT INTO items (item_id, name, price, image) VALUES (?, ?, ?, ?)", (self.rand_item_id(), name, price, photo))
+        self.__db.commit()
+
+    def rand_item_id(self):
+        """Генерирование случайного id товара"""
+        last_id = self.get_last_item_id()
+        new_id = last_id[:3]+str(int(last_id[2:])+1)
+        while len(new_id) < 9:
+            new_id = new_id[:3]+ '0' + new_id[3:]
+        return new_id
+
+    def get_last_item_id(self):
+        """Получение последнего зарегестрированного id"""
+        cursor = self.__db.cursor()
+        users = cursor.execute("SELECT * FROM items").fetchall()
+        return users[len(users)-1][0]
+
+    def get_item_photo(self, item_id):
+        """Получение фото товара"""
+        cursor = self.__db.cursor()
+        image = cursor.execute("SELECT image FROM items WHERE item_id=?", (item_id,)).fetchone()[0]
+        return image
+
+    def get_item_name(self, item_id):
+        """Получение названия товара"""
+        cursor = self.__db.cursor()
+        name = cursor.execute("SELECT name FROM items WHERE item_id=?", (item_id,)).fetchone()[0]
+        return name
+
+    def get_item_price(self, item_id):
+        """Получение цены товара"""
+        cursor = self.__db.cursor()
+        price = cursor.execute("SELECT price FROM items WHERE item_id=?", (item_id,)).fetchone()[0]
+        print(price)
+        return price
+
+
+
+    #Методы для заказов
+    def get_all_oredrs(self):
+        """Получение списка всех заказов"""
+        cursor = self.__db.cursor()
+        return cursor.execute("SELECT * FROM orders").fetchall()[::-1]
+
+
+
+    #Методы для корзины
+    def add_item_in_cart(self, user_id, item_id):
+        """Добавление товара в корзину"""
+        cursor = self.__db.cursor()
+        items = cursor.execute("SELECT items FROM cart where user_id=?", (user_id,)).fetchone()
+        if items != None:
+            new_items = items[0] + ' ' + item_id
+            cursor.execute("UPDATE cart SET items=? WHERE user_id = ?", (new_items, user_id))
+        else:
+            new_items = item_id
+            cursor.execute("INSERT INTO cart (user_id, items) VALUES (?, ?)", (user_id, new_items))
+        self.__db.commit()
+
+    def get_user_cart(self, user_id):
+        """Получение корзины пользователя"""
+        cursor = self.__db.cursor()
+        items = cursor.execute("SELECT items FROM cart where user_id=?", (user_id,)).fetchone()
+        if items != None:
+            return items[0].split()
+        else:
+            return []
+
+    def erase_item_from_cart(self, user_id, item_id):
+        """Удаление товара из корзины"""
+        cursor = self.__db.cursor()
+        items = cursor.execute("SELECT items FROM cart where user_id=?", (user_id,)).fetchone()
+        if items != None:
+            new_items = items[0].split()
+            new_items.remove(item_id)
+            cursor.execute("UPDATE cart SET items=? WHERE user_id = ?", (new_items, user_id))
+        self.__db.commit()
+
+    def is_item_in_cart(self, user_id, item_id):
+        """Проверка на наличие товара в корзине"""
+        cursor = self.__db.cursor()
+        items = cursor.execute("SELECT items FROM cart where user_id=?", (user_id,)).fetchone()
+        if items != None:
+            itmes_lst = items[0].split()
+            if item_id in itmes_lst:
+                return True
+            else:
+                return False
+        else:
+            return False
+            
+
+
+    #Методы дляь пользователей
+    def get_last_id(self):
+        """Получение последнего зарегестрированного id"""
+        cursor = self.__db.cursor()
+        users = cursor.execute("SELECT * FROM users").fetchall()
+        return users[len(users)-1][0]
+    
+    def get_user_quantity(self):
+        """Получение количества пользователей"""
+        cursor = self.__db.cursor()
+        return len(cursor.execute("SELECT * FROM users").fetchall())
+
+    def get_user_name(self, id):
+        """Получение имени пользователя по id"""
+        cursor = self.__db.cursor()
+        return cursor.execute("SELECT name FROM users WHERE user_id=?", (id,)).fetchone()[0]
+
+    def get_user_phone(self, id):
+        """Получение имени пользователя по id"""
+        cursor = self.__db.cursor()
+        return cursor.execute("SELECT phone_number FROM users WHERE user_id=?", (id,)).fetchone()[0]
+
+    def is_admin(self, id):
+        """Проверка на права"""
+        cursor = self.__db.cursor()
+        if cursor.execute("SELECT admin FROM users WHERE user_id=?", (id,)).fetchone()[0] == 1:
+            return True
+        else:
+            return False
+
+    def rand_user_id(self):
+        """Генерирование случайного id пользователя"""
+        last_id = self.get_last_id()
+        new_id = last_id[:3]+str(int(last_id[2:])+1)
+        while len(new_id) < 9:
+            new_id = new_id[:3]+ '0' + new_id[3:]
+        return new_id
+
+    def add_user(self, name, phone, login, password):
+        """Добавление нового пользователя"""
+        cursor = self.__db.cursor()
+        cursor.execute("INSERT INTO users (user_id, name, phone_number, admin, login, password) VALUES (?, ?, ?, ?, ?, ?)", (self.rand_user_id(), name, phone, 0, login, password))
+        self.__db.commit()
+
+    def password_check(self, login, password):
+        """Проверка пароля и возвращение id"""
+        cursor = self.__db.cursor()
+        if cursor.execute("SELECT password FROM users WHERE login=?", (login,)).fetchone()[0] == password:
+            return cursor.execute("SELECT user_id FROM users WHERE login=?", (login,)).fetchone()[0]
+        else:
+            return None
+
+    def is_registered(self, login):
+        """Проверка на регистрацию"""
+        cursor = self.__db.cursor()
+        if cursor.execute("SELECT password FROM users WHERE login=?", (login,)).fetchone() != None:
+            return True
+        else: 
+            return False
+
+if __name__ == '__main__':
+    reader = DbReader()
+    print(reader.is_registered('admin@am.com'))
---
+++ app.py
@@ -0,0 +1,187 @@
+﻿from flask import Flask, render_template, redirect, request
+from DB_reader import DbReader
+
+app = Flask(__name__)
+reader = DbReader()
+local_id = None
+local_cart = []
+
+def str_to_str(items):
+    items = items.split()
+    dict_items = {}
+    result = ''
+    for i in items:
+        if i in dict_items.keys():
+            dict_items[i] += 1
+        else:
+            dict_items[i] = 1
+
+    for i in dict_items.keys():
+        result += str(dict_items[i]) + 'x ' + i + ', '
+
+    result = result[:len(result)-2]
+    result += '.'
+    return result
+
+def str_to_dict(items):
+    items = items.split()
+    dict_items = {}
+    for i in items:
+        if i in dict_items.keys():
+            dict_items[i] += 1
+        else:
+            dict_items[i] = 1
+    return dict_items
+
+
+@app.route('/add_item_in_cart/<id>', methods = ['POST', 'GET'])
+def add_item_in_cart(id):
+    if local_id == None:
+        local_cart.append(id)
+    else:
+        reader.add_item_in_cart(local_id, id)
+    return redirect('/')
+
+@app.route('/erase_item_from_cart/<id>', methods = ['POST', 'GET'])
+def erase_item_from_cart(id):
+    if local_id == None:
+        if id in local_cart:
+            local_cart.remove(id)
+    else:
+        reader.erase_item_in_cart(local_id, id)
+    return redirect('/')
+
+@app.route('/')
+def index():
+    global local_id
+    global local_cart
+    items = reader.get_all_items()
+    items_info = []
+    if local_id != None:
+        for i in items:
+            item_info = []
+            item_info.append(i)
+            if reader.is_item_in_cart(local_id, i[0]):
+                item_info.append(['В корзине', '/erase_item_from_cart/', 'btn btn-success'])
+            else:
+                item_info.append(['В корзину', '/add_item_in_cart/', 'btn btn-primary'])
+            items_info.append(item_info)
+
+        if reader.is_admin(local_id) == True:
+            header_tegs = [['Главная', '#', "nav-link px-2 link-secondary"], ['Заказы', '/orders', 'nav-link px-2'], ['Добавление товара', '/create_new_item', 'nav-link px-2']]
+            header_buttons = [[reader.get_user_name(local_id), '#', 'btn btn-primary'], ['Выйти', '/exit', "btn btn-outline-primary"]]
+        else:
+            header_tegs = [['Главная', '#', "nav-link px-2 link-secondary"], ['Корзина', '/cart', 'nav-link px-2']]
+            header_buttons = [[reader.get_user_name(local_id), '#', 'btn btn-primary'], ['Выйти', '/exit', "btn btn-outline-primary"]]
+    else:
+        for i in items:
+            item_info = []
+            item_info.append(i)
+            if i[0] in local_cart:
+                 item_info.append(['В корзине', '/erase_item_from_cart/', 'btn btn-success'])
+            else:
+                item_info.append(['В корзину', '/add_item_in_cart/', 'btn btn-primary'])
+            items_info.append(item_info)
+
+        header_tegs = [['Главная', '#', 'nav-link px-2 link-secondary'], ['Корзина', '/cart', 'nav-link px-2']]
+        header_buttons = [['Войти', '/sign_in', 'btn btn-outline-primary'], ['Зарегистрироваться', '/registration', "btn btn-primary"]]
+    
+    return render_template('index.html', items=items_info, header_tegs=header_tegs, header_buttons=header_buttons)
+
+
+@app.route('/sign_in', methods=['POST', 'GET'])
+def sign_in():
+    global local_id
+    if request.method == 'POST':
+        login = request.form['login']
+        password = request.form['password']
+        if reader.is_registered(login):
+            local_id = reader.password_check(login, password)
+            if local_id != None:
+                return redirect('/')
+            else:
+                return render_template('sign_in.html')
+        else:
+            return render_template('sign_in.html')
+    else:
+        return render_template('sign_in.html')
+
+@app.route('/create_new_item', methods=['POST', 'GET'])
+def create_new_item():
+    if request.method == 'POST':
+        item_name = request.form['item_name']
+        item_photo = request.form['item_photo']
+        item_price = request.form['item_price']
+        reader.add_item(item_name, item_price, item_photo)
+        return redirect('/')
+    else:
+        return render_template('create_new_item.html')
+
+@app.route('/registration', methods=['POST', 'GET'])
+def registration():
+    global local_id
+    if request.method == 'POST':
+        name = request.form['name']
+        phone = request.form['phone']
+        login = request.form['login']
+        password = request.form['password']
+        if not reader.is_registered(login):
+            reader.add_user(name, phone, login, password)
+            local_id = reader.password_check(login, password)
+            return redirect('/')
+        else:
+            return render_template('registration.html')
+    else:
+        return render_template('registration.html')
+
+@app.route('/orders')
+def orders():
+    orders = reader.get_all_oredrs()[::-1]
+    data = []
+    for i in orders:
+        order_info = []
+        order_info.append(i[4])
+        order_info.append(reader.get_user_name(i[0]))
+        order_info.append(reader.get_user_phone(i[0]))
+        items = str_to_str(i[1])
+        order_info.append(items)
+        order_info.append(i[3])
+        if i[2] == 1:
+            order_info.append(['Обработано', '#', "btn btn-success"])
+        else:
+            order_info.append(['Обработать', '/processin_order', "btn btn-warning"])
+        data.append(order_info)
+    return render_template('orders.html', data=data)
+
+@app.route('/cart')
+def cart():
+    global local_id
+    global local_cart
+    new_items = {}
+    items_info = []
+    if local_id == None:
+        items = local_cart
+    else:
+        items = reader.get_user_cart(local_id)
+    for i in items:
+        if i in new_items.keys():
+            new_items[i] += 1
+        else:
+            new_items[i] = 1
+
+    total_sum = 0
+    for i in new_items.keys():
+        items_info.append([i, new_items[i], reader.get_item_name(i), reader.get_item_photo(i), reader.get_item_price(i)])
+        total_sum += reader.get_item_price(i)
+    return render_template('cart.html', items=items_info, sum=total_sum)
+
+@app.route('/exit')
+def exit():
+    global local_id
+    global local_cart
+    local_id = None
+    local_cart.clear()
+    return redirect('/')
+
+if __name__ == "__main__":
+    app.run(debug=True)
